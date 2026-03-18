fbd625622d2b14545ca33fdc058786c1b41b07a5
samroach <samroach03@gmail.com>
2026-03-18 12:48:21 -0500

    Update venue.gd

--- venue.gd
+++ venue.gd
@@ -1,14 +1,25 @@
 extends Control
 
 signal crate_selected
+signal setlist_selected
 signal venue_selected(venue_data)
 
 @onready var crate_button = $WorldShell/RecordStore/GoToCrateDig
 @onready var crowd_label = $WorldShell/CrowdSummary
+@onready var setlist_button = $WorldShell/RecordStore/GoToSetlist
 
 var venue_buttons: Array = []
+var venue_lights: Array = []
 var venue_data: Array = []
 var flash_time := 0.0
+
+const GENRE_COLORS = {
+	"Euro": Color(0.1, 0.9, 0.9, 1.0),
+	"RnB": Color(0.3, 0.4, 1.0, 1.0),
+	"Pop": Color(1.0, 0.5, 0.8, 1.0),
+	"Hiphop": Color(1.0, 0.0, 0.0, 1.0),
+	"EDM": Color(0.3, 1.0, 0.3, 1.0)
+}
 
 func _ready() -> void:
 	venue_buttons = [
@@ -16,7 +27,22 @@ func _ready() -> void:
 		$WorldShell/VenueRoadmap/Venue2Panel/GoToV2,
 		$WorldShell/VenueRoadmap/Venue3Panel/GoToV3
 	]
+	venue_lights = [
+		[
+			$WorldShell/VenueRoadmap/Venue1Panel/GenreLights1/GenreLightA,
+			$WorldShell/VenueRoadmap/Venue1Panel/GenreLights1/GenreLightB
+		],
+		[
+			$WorldShell/VenueRoadmap/Venue2Panel/GenreLights2/GenreLightA,
+			$WorldShell/VenueRoadmap/Venue2Panel/GenreLights2/GenreLightB
+		],
+		[
+			$WorldShell/VenueRoadmap/Venue3Panel/GenreLights3/GenreLightA,
+			$WorldShell/VenueRoadmap/Venue3Panel/GenreLights3/GenreLightB
+		]
+	]
 	crate_button.pressed.connect(func(): crate_selected.emit())
+	setlist_button.pressed.connect(func(): setlist_selected.emit())
 	for i in range(venue_buttons.size()):
 		venue_buttons[i].pressed.connect(_on_venue_pressed.bind(i))
 
@@ -27,6 +53,7 @@ func setup_world(options: Array, crowd_state: Dictionary, can_perform: bool = fa
 		var button = venue_buttons[i]
 		if i >= venue_data.size():
 			button.hide()
+			_update_light_pair(i, [])
 			continue
 		button.show()
 		var v = venue_data[i]
@@ -34,32 +61,41 @@ func setup_world(options: Array, crowd_state: Dictionary, can_perform: bool = fa
 		button.text = "%s\n%s" % [v.get("name", "Venue"), "/".join(genres)]
 		button.disabled = not can_perform
 		button.tooltip_text = "Pick a venue after building a 5-song setlist." if not can_perform else "Start the show"
+		_update_light_pair(i, genres)
 
 func _on_venue_pressed(index: int):
 	if index < venue_data.size():
 		venue_selected.emit(venue_data[index])
 
 func _process(delta: float) -> void:
-	flash_time += delta * 3.5
+	flash_time += delta * 4.0
 	for i in range(venue_buttons.size()):
 		if i >= venue_data.size():
 			continue
-		var button = venue_buttons[i]
 		var genres = venue_data[i].get("genres", ["Unknown"])
-		var energy = 0.6 + (sin(flash_time + i) + 1.0) * 0.2
-		button.modulate = _genre_color(genres[0], energy)
+		_animate_light_pair(i, genres)
+
+func _update_light_pair(index: int, genres: Array) -> void:
+	if index >= venue_lights.size():
+		return
+	for j in range(venue_lights[index].size()):
+		var light: ColorRect = venue_lights[index][j]
+		if j < genres.size():
+			light.visible = true
+			light.color = _genre_color(genres[j], 0.45)
+		else:
+			light.visible = false
+
+func _animate_light_pair(index: int, genres: Array) -> void:
+	if index >= venue_lights.size():
+		return
+	for j in range(venue_lights[index].size()):
+		var light: ColorRect = venue_lights[index][j]
+		if j >= genres.size():
+			continue
+		var pulse = 0.45 + 0.55 * (0.5 + 0.5 * sin(flash_time * (1.1 + j * 0.2) + index + j))
+		light.color = _genre_color(genres[j], pulse)
 
 func _genre_color(genre: String, alpha: float) -> Color:
-	match genre:
-		"Euro":
-			return Color(0.1, 0.9, 0.9, alpha)
-		"RnB":
-			return Color(0.3, 0.4, 1.0, alpha)
-		"Pop":
-			return Color(1.0, 0.5, 0.8, alpha)
-		"Hiphop":
-			return Color(1.0, 0.3, 0.3, alpha)
-		"EDM":
-			return Color(0.5, 1.0, 0.3, alpha)
-		_:
-			return Color(1.0, 1.0, 1.0, alpha)
+	var base: Color = GENRE_COLORS.get(genre, Color.WHITE)
+	return Color(base.r, base.g, base.b, alpha)
